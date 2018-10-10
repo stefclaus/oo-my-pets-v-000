8936c030f73a28b9185d13b627014f6f8c8bc326
Stef Claus <stef.claus@gmail.com>
2018-10-10 02:46:29 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+
+attr_accessor :name 
+attr_reader :species 
+
+@@all = []
+@@count = 0 
+def initialize(name) 
+  @name = name 
+  @@all << self 
+  @@count += 1
+end 
+
+def self.all
+  @@all 
+end 
+
+def self.reset_all
+  @@all = []
+  @@count = 0 
+end 
+
+def self.count 
+  @@count 
+end 
+
 end
