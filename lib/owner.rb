b1082e528e2af26227cfd78735557591e1414be1
Stef Claus <stef.claus@gmail.com>
2018-10-10 03:17:26 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,22 @@
 class Owner
 
-attr_accessor :name 
+attr_accessor :name, :pets, :fishes 
 attr_reader :species 
 
 @@all = []
 @@count = 0 
+
+
 def initialize(name) 
   @name = name 
   @@all << self 
   @@count += 1
+  @species = "human"
+  @pets = {:cats => [], :dogs => [], :fishes => [] }
+end 
+
+def say_species 
+  "I am a human." 
 end 
 
 def self.all
@@ -23,5 +31,10 @@ end
 def self.count 
   @@count 
 end 
+
+def buy_fish
+  Fish.new 
+end 
+
 
 end
