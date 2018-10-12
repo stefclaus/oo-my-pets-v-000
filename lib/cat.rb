1a2ee350ca478da11a43c3ff928e126e64f5d048
Stef Claus <stef.claus@gmail.com>
2018-10-12 00:47:20 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,6 +3,7 @@ class Cat
 attr_accessor :mood 
 attr_reader :name 
 
+
 def initialize(name)
   @name = name
   @mood = "nervous" 
--- lib/owner.rb
+++ lib/owner.rb
@@ -33,12 +33,20 @@ def self.count
 end 
 
 
-def buy_fish(fish)
-  @@all << fish
-  fish.owner = self 
+def buy_fish(name)
+  new_fish = Fish.new(name)
+  @pets[:fishes] << new_fish
 end 
 
- 
+ def buy_cat(name)
+  new_cat = Cat.new(name)
+  @pets[:cats] << new_cat
+end 
+
+def buy_dog(name)
+  new_dog = Dog.new(name)
+  @pets[:dogs] << new_dog 
+end 
 
 
 end
